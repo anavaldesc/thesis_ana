1682595eeac85a987e3ad70eb8843cb9a4da9804
Ana Valdes <anavaldesc@gmail.com>
2019-09-09 19:40:18 -0400

    Chapter 8
    -Wrote stuff about shears
    -Changed Eq. to Equation, Fig. to Figure
    -Minor changes to Fourier spectroscopy section
    -Made figure with grid of psd for all initial and final states in Fourier spectroscopy

--- Figure_scripts/rashba_floquet.py
+++ Figure_scripts/rashba_floquet.py
@@ -19,7 +19,7 @@ import matplotlib.pyplot as plt
 from scipy.linalg import eigvalsh
 from matplotlib.gridspec import GridSpec
 from matplotlib import rcParams
-
+#%%
 
 fontsize = 9
 rcParams['axes.labelsize'] = fontsize
@@ -54,7 +54,7 @@ rcParams['ytick.minor.size'] = 2      # minor tick size in points
 rcParams['ytick.major.width'] = 0.75       # major tick width in points
 rcParams['ytick.minor.width'] = 0.75      # minor tick width in points
 
-
+#%%
 def H_RashbaRF(qx, qy, 
                Omega1, Omega2, Omega3, 
                delta1, delta2, delta3,
@@ -136,7 +136,8 @@ def block_matrix(n_bands, n_block):
     block = np.eye(n_bands+n_block)[n_block::,0:-n_block]
     return block + block.T
 
-def Rashba_floquet(kx, ky, n_manifolds, omega_fl, Omega_zx, Omega_xy, Omega_zy):
+def Rashba_floquet(kx, ky, n_manifolds, omega_fl, Omega_zx, Omega_xy, Omega_zy,
+                   off_resonant=False):
 
     kwargs = {'Omega1':0, 'Omega2':0, 'Omega3':0, 
               'delta1':0, 'delta2':0, 'delta3':0}
@@ -157,9 +158,13 @@ def Rashba_floquet(kx, ky, n_manifolds, omega_fl, Omega_zx, Omega_xy, Omega_zy):
     mat_zy = np.kron(block_matrix(n_bands, 3), f_zy)
     mat_xy = np.kron(block_matrix(n_bands, 1), f_xy)
     
-    mat_zx_off = np.kron(block_matrix(n_bands, 3), f_zx)
+    mat_zx_off = np.kron(block_matrix(n_bands, 3), f_zx) / 1.5
+    mat_zy_off = np.kron(block_matrix(n_bands, 2), f_zy) / 1.5
     
-    H_floquet = floquet_diag + mat_zx + mat_xy + mat_zy #+ mat_zx_off
+    
+    H_floquet = floquet_diag + mat_zx + mat_xy + mat_zy 
+    if off_resonant:
+        H_floquet += mat_zx_off + mat_zy_off
     
     return H_floquet
 
@@ -180,9 +185,17 @@ for i in range(16, 19):
     ee = floquet_eigenarray(i, **kwargs)
 #    plt.plot(ee, 'k')
     e_floquet.append(ee)
-#plt.show()
+    
+e_floquet_off = []
+
+kwargs = {'kx':q, 'ky':0, 'n_manifolds':6, 'Omega_zx':Om, 'Omega_xy':Om, 
+          'Omega_zy':Om, 'omega_fl':1*83.24/3.678, 'off_resonant':True}
 
 e_floquet_large = []
+for i in range(16, 19):
+    ee = floquet_eigenarray(i, **kwargs)
+#    plt.plot(ee, 'k')
+    e_floquet_off.append(ee)
 
 kwargs = {'kx':q, 'ky':0, 'n_manifolds':6, 'Omega_zx':Om, 'Omega_xy':Om, 
           'Omega_zy':Om, 'omega_fl':5*83.24/3.678}
@@ -222,6 +235,40 @@ plt.ylim([0,1])
 ax.set_yticklabels([])
 plt.tight_layout()
 plt.savefig('floquet_effects.pdf')
+
+#%%
+
+fig = plt.figure(figsize=(7/1.7,4./1.7))
+gs = GridSpec(1, 2)   
+ax = plt.subplot(gs[0])    
+    
+e_floquet = np.array(e_floquet)
+e_floquet -= e_floquet.min()
+e_floquet_large = np.array(e_floquet_large)
+e_floquet_large -= e_floquet_large.min()
+e_floquet_off = np.array(e_floquet_off)
+e_floquet_off -= e_floquet_off.min()
+e_three = np.array(e_three)
+e_three -= e_three.min()
+plt.plot(q, e_three.T, 'k-', label='effective Hamiltonian')
+plt.plot(q, e_floquet.T, 'k--', label='Floquet Hamiltonian')
+plt.xlabel('$q_x$, $\mathrm{in\ units\ of\ } k_L$')
+plt.ylabel('$\mathrm{Energy\ in\ units\ of\ } E_{\mathrm{L}}$')
+#plt.legend(loc='upper center')
+plt.ylim([0,1])
+plt.yticks([0,0.5,1])
+ax = plt.subplot(gs[1])   
+
+plt.plot(q, e_three.T, 'k-', label='effective Hamiltonian')
+plt.plot(q, e_floquet_off.T, 'k--', label='Floquet Hamiltonian')
+plt.xlabel('$q_x$, $\mathrm{in\ units\ of\ } k_L$')
+#plt.ylabel('Energy in units of $E_L$')
+#plt.legend(loc='upper right')
+plt.ylim([0,1]) 
+ax.set_yticklabels([])
+plt.tight_layout()
+plt.savefig('floquet_effects.pdf')
+
 
 #%%
 
