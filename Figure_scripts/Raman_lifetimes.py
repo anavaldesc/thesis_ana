f6b84e07028d307fb0c4fb8e5642ea93b388f4b5
Ana Valdes <anavaldesc@gmail.com>
2019-09-24 19:45:15 -0400

    Rabi and Ramsey
    A bit of Floquet, one new pretty figure

--- Figure_scripts/Raman_lifetimes.py
+++ Figure_scripts/Raman_lifetimes.py
@@ -60,16 +60,20 @@ rcParams['ytick.major.width'] = 0.75       # major tick width in points
 rcParams['ytick.minor.width'] = 0.75      # minor tick width in points
 
 #%%
-Gamma_D2 = 38.11e-6 # 1/s
-Gamma_D1 = 36.10e-6  # 1/s
+Gamma_D2 = 38.11e6 # 1/s
+Gamma_D1 = 36.10e6  # 1/s
+#I_sat = 
 
 def alpha(wavelength):
     c = 2.99792458 * 1e8
+    epsilon_0 = 8.854187817e-12
     omega = 2 * np.pi * c / wavelength * 1e-9
     omega_2  = 2 * np.pi * c / 780.24*1e-9
     omega_1 = 2 * np.pi * c / 794.98*1e-9
-    alpha = 2 / (omega - omega_2) + 1 / (omega - omega_1)
-
+    alpha = 1 * 2 / omega_2**3 * (1 / (omega - omega_2) + 0. / (omega + omega_2))
+    alpha +=  1 /omega_1**3 * (1/(omega - omega_1)+0./(omega + omega_1))
+    alpha *= np.pi * c**2/2 * (Gamma_D1 + Gamma_D2)/2
+    
     return alpha
 
 def alpha_v(wavelength):
@@ -118,11 +122,11 @@ fig = plt.figure(figsize=(5.5,2.25))
 gs = GridSpec(1, 2)   
 ax = plt.subplot(gs[0])
 
-plt.semilogy(wavelengths, np.abs(alphas_v)*1e-9*1.45/3.678)
+plt.semilogy(wavelengths, np.abs(alphas_v)*1e-41*1.45/3.678)
 plt.semilogy(lambdas, Omegas_norm/3.278, 'o', mec='k', ms=7, mfc='lightgray')
 plt.xlabel('Wavelength [nm]')
 plt.ylabel('Raman coupling strength $[E_{\mathrm{L}}]$')
-plt.ylim([1, 10**2])
+#plt.ylim([1, 10**2])
 #ax2 = ax.twinx()
 #ax2.semilogy(wavelengths, np.abs(alphas)*1e-4*1.45/3.678, 'k')
 
@@ -141,11 +145,41 @@ plt.ylabel('$1/e$ lifetime [s]')
 plt.xlim([wavelengths.min(), wavelengths.max()])
 #plt.ylim([-100, 10**4])
 plt.tight_layout()
-plt.savefig('Raman_vs_lambda.pdf')
+#plt.savefig('Raman_vs_lambda.pdf')
 
 #%%
 
-#wavelengths = np.linspace(775, 800, 1e6)
+from scipy.constants import hbar
+fig = plt.figure(figsize=(5.5,2.25))
+#    
+gs = GridSpec(1, 1)   
+ax = plt.subplot(gs[0])
+
+wavelengths = np.linspace(775, 800, int(1e3))
+alphas = alpha(wavelengths)
+plt.plot(wavelengths, (alphas)*1e-40)
+y_lim = 0.1
+plt.hlines(0,wavelengths.min(), wavelengths.max(), linestyle='--' )
+plt.ylim([-y_lim, y_lim])
+
+#plt.ylim([-10, 10**5])
+#plt.hlines(alpha_min, wavelengths.min(), wavelengths.max(),
+#           linestyle='--')
+#plt.hlines(5 * alpha_min, wavelengths.min(), wavelengths.max(),
+#           linestyle='-.')
+#plt.axis('Tight')
+
+#min_idx = np.argmin(np.abs(alphas))
+
+#plt.vlines(532, 0, 10**11,
+#           linestyle='--')
+#plt.vlines(1064, 0, 10**11,
+#           linestyle='--')
+plt.xlim([wavelengths.min(), wavelengths.max()])
+plt.xlabel('Wavelength [nm]')
+
+#ax = plt.subplot(gs[1])
+#wavelengths = np.linspace(775, 800, int(1e6))
 #alphas = alpha(wavelengths)
 #plt.semilogy(wavelengths, np.abs(alphas))
 #
